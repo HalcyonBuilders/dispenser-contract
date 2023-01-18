54ce3e0fc83f5c324a70274062beb8f1a2dea644
ThounyBreasty <thouny@tuta.io>
2023-01-18 16:15:08 +0100

    eslint fix, snippets fixed waiting for MystenLabs

--- snippets/src/index.ts
+++ snippets/src/index.ts
@@ -1,38 +1,32 @@
-import { Ed25519Keypair, JsonRpcProvider, RawSigner, Base64DataBuffer, Network } from "@mysten/sui.js";
+import { Ed25519Keypair, JsonRpcProvider, RawSigner, Network } from "@mysten/sui.js";
 import { execSync } from "child_process";
-import fs from "fs";
-import { join } from "path";
+// import fs from "fs";
 
-(async() => {
-
+(async () => {
     console.log("running...");
-    
+
     const cliPath = "/home/titouanmarchal/.cargo/bin/sui";
     const packagePath = "/home/titouanmarchal/ThounyBreasty/Projects/Sui/Halcyon/Dispenser/contract";
     // Generate a new Keypair
     const keypair = Ed25519Keypair.deriveKeypair(
-        "found round original fancy funny gloom cushion mask stairs pact legal open"
+        "stick trend survey toy steel neutral bus hamster delay apple solar vague"
     );
     const provider = new JsonRpcProvider(Network.DEVNET);
+    const signer = new RawSigner(keypair, provider);
+
     const address = keypair.getPublicKey().toSuiAddress();
     console.log(address);
-    
-    const signer = new RawSigner(keypair, provider);
+
     const compiledModules = JSON.parse(
         execSync(
             `${cliPath} move build --dump-bytecode-as-base64 --path ${packagePath}`,
             { encoding: "utf-8" }
         )
     );
-    
-    // const modulesInBytes = compiledModules.map((m: (string | Uint8Array)) =>
-    //     Array.from(new Base64DataBuffer(m).getData())
-    // );
-    
+
     const publishTxn = await signer.publish({
-        compiledModules: compiledModules,
-        gasBudget: 1000000,
+        compiledModules,
+        gasBudget: 10000,
     });
     console.log("publishTxn", publishTxn);
-    return;
-})();
+})()
